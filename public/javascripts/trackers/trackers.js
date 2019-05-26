f6831af62e66332832f2885e0d3d5ac4c2507653
Seth Kellas <sjkellas@gmail.com>
2019-05-25 20:34:05 -0400

    Externalize js methods

---
+++ public/javascripts/trackers/trackers.js
@@ -0,0 +1,56 @@
+$(function(){
+    // Button Management
+    $('#trackerId').on('change', function() {
+        $('button#create').hide();
+        $('button#update').show();
+    });
+    $('button#update').on('click', function(event) {
+        data = $('#trackerForm').serializeArray();
+        $.ajax({
+        url: "/trackers/"+data[0].value,
+        type: 'PUT',
+        dataType: "json",
+        data: data
+        });
+    });
+    $('button#create').on('click', function(event) {
+        $.ajax({
+        url: "/trackers/",
+        type: 'POST',
+        dataType: "json",
+        data: $('#trackerForm').serializeArray(),
+        success: function(data) {
+            console.log(data);
+            $('#trackerMenu span').append(`
+            <p><a class="tracker" data-id="${data.id}" href="#">${data.name}</a></p>
+            `);
+            // TODO: Need to rebind
+        }
+        });
+    });
+
+    // Tracker Menu Management
+    $('a.new-tracker').on('click', function() {
+        $('#trackerId').val(null);
+        $('#trackerName').val('');
+        $('#trackerDescription').val('');
+        $('#trackerScale').val('');
+
+        $('button#create').show();
+        $('button#update').hide();
+    });
+    $('a.tracker').on('click', function (event) {
+        event.preventDefault();
+        $.ajax({
+        url: "/trackers/"+$(this).data('id'),
+        type: 'GET',
+        dataType: "json",
+        success: function(data) {
+            $('#trackerId').val(data.id).trigger('change');
+            $('#trackerName').val(data.name);
+            $('#trackerDescription').val(data.description);
+            $('#trackerScale').val(data.scale);
+        }
+        });
+    });
+  });
